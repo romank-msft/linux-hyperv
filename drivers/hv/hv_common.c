7b55dcfe208b2999caecb541c1c4dd8aa5557dc1
Roman Kisel <romank@linux.microsoft.com>
2025-06-02 14:50:59 -0700

    arch: hyperv: Get/set SynIC synth.registers via paravisor
    The existing Hyper-V wrappers for getting and setting MSRs are
    hv_get/set_msr(). Via hv_get/set_non_nested_msr(), they detect
    when running in a CoCo VM with a paravisor, and use the TDX or
    SNP guest-host communication protocol to bypass the paravisor
    and go directly to the host hypervisor for SynIC MSRs. The "set"
    function also implements the required special handling for the
    SINT MSRs.
    
    But in some Confidential VMBus cases, the guest wants to talk only
    with the paravisor. To accomplish this, provide new functions for
    accessing SynICs that always do direct accesses (i.e., not via
    TDX or SNP GHCB), which will go to the paravisor. The mirroring
    of the existing "set" function is also not needed. These functions
    should be used only in the specific Confidential VMBus cases that
    require them.
    
    Provide functions that allow manipulating the SynIC registers
    through the paravisor.
    
    Signed-off-by: Roman Kisel <romank@linux.microsoft.com>
    Reviewed-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- drivers/hv/hv_common.c
+++ drivers/hv/hv_common.c
@@ -716,6 +716,19 @@ u64 __weak hv_tdx_hypercall(u64 control, u64 param1, u64 param2)
 }
 EXPORT_SYMBOL_GPL(hv_tdx_hypercall);
 
+int __weak hv_para_get_synic_register(unsigned int reg, u64 *val)
+{
+	*val = ~0ULL;
+	return -ENODEV;
+}
+EXPORT_SYMBOL_GPL(hv_para_get_synic_register);
+
+int __weak hv_para_set_synic_register(unsigned int reg, u64 val)
+{
+	return -ENODEV;
+}
+EXPORT_SYMBOL_GPL(hv_para_set_synic_register);
+
 void hv_identify_partition_type(void)
 {
 	/* Assume guest role */
