065c0f78669750a51f7e4c7923e95d20d654db9a
Roman Kisel <romank@linux.microsoft.com>
2025-07-14 15:15:33 -0700

    arch/x86: mshyperv: Trap on access for some synthetic MSRs
    hv_set_non_nested_msr() has special handling for SINT MSRs
    when a paravisor is present. In addition to updating the MSR on the
    host, the mirror MSR in the paravisor is updated, including with the
    proxy bit. But with Confidential VMBus, the proxy bit must not be
    used, so add a special case to skip it.
    
    Signed-off-by: Roman Kisel <romank@linux.microsoft.com>
    Reviewed-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Reviewed-by: Tianyu Lan <tiala@microsoft.com>

--- include/asm-generic/mshyperv.h
+++ include/asm-generic/mshyperv.h
@@ -300,6 +300,7 @@ bool hv_isolation_type_snp(void);
 bool hv_confidential_vmbus_available(void);
 u64 hv_ghcb_hypercall(u64 control, void *input, void *output, u32 input_size);
 u64 hv_tdx_hypercall(u64 control, u64 param1, u64 param2);
+void hv_para_set_sint_proxy(bool enable);
 int hv_para_get_synic_register(unsigned int reg, u64 *val);
 int hv_para_set_synic_register(unsigned int reg, u64 val);
 void hyperv_cleanup(void);
